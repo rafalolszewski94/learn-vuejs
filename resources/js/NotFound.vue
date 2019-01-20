280ef7b526b5ac8da392182814e2ec0e6e1d65e3
Rafał Olszewski <olszak94@gmail.com>
2019-01-20 21:39:53 +0100

    Removed cypress (unused)

--- resources/js/NotFound.vue
+++ resources/js/NotFound.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col items-center justify-center w-full min-w-full min-h-screen">
-    <h1 class="font-heading text-5xl font-black text-indigo">404</h1>
-    <p class="font-heading font-medium">Not found</p>
+    <h1 class="font-heading text-display-4 font-black text-indigo">404</h1>
+    <p class="font-heading font-medium">Page not found</p>
 
     <router-link class="no-underline text-black text-xs uppercase mt-4 px-3 py-2 rounded border hover:bg-grey-light" :to="{name: 'Home'}">Go to home</router-link>
   </div>
--- tailwind.js
+++ tailwind.js
@@ -270,8 +270,13 @@ module.exports = {
     '2xl': '1.5rem',    // 24px
     '3xl': '1.875rem',  // 30px
     '4xl': '2.25rem',   // 36px
-    '5xl': '3rem',      // 48px
-    'display': '8rem',      // 48px
+    '5xl': '3rem',
+    '6xl': '4rem',
+    '7xl': '5rem',
+    'display': '8rem',
+    'display-2': '10rem',
+    'display-3': '12rem',
+    'display-4': '14rem',
   },
 
 
